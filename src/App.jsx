78bb4a51e96544f5f848ac7a80204ed42f116d3a
Paresh <09patilparesh@gmail.com>
2025-04-23 00:01:35 +0530

    gallery Page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import Navbar from "./Components/shared/Navbar";
 import Footer from "./Components/shared/Footer";
-import Login from "./Pages/Login";
-import About from "./Pages/About";
-import MembershipArea from "./Pages/MembershipArea";
+import Navbar from "./Components/shared/Navbar";
 import BlogsSection1 from "./Components/specific/Blogs/BlogsSection1";
 import BlogsSection2 from "./Components/specific/Blogs/BlogsSection2";
-import ContactUs from "./Pages/ContactUs";
 import MembershipFormforIndividualAnnual from "./Components/specific/Forms/MembershipFormforIndividualAnnual";
+import About from "./Pages/About";
+import ContactUs from "./Pages/ContactUs";
+import GalleryPage from "./Pages/Gallery";
+import Home from "./Pages/Home";
+import MembershipArea from "./Pages/MembershipArea";
 
 const App = () => {
   return (
@@ -18,11 +18,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/MembershipArea" element={<MembershipArea/>} />
-        <Route path="/BlogsSection1" element={<BlogsSection1/>} />
-        <Route path="/BlogsSection2" element={<BlogsSection2/>} />
-        <Route path="/ContactUs" element={<ContactUs/>} />
-        <Route path="/MembershipFormforIndividualAnnual" element={<MembershipFormforIndividualAnnual/>} />
+        <Route path="/MembershipArea" element={<MembershipArea />} />
+        <Route path="/BlogsSection1" element={<BlogsSection1 />} />
+        <Route path="/BlogsSection2" element={<BlogsSection2 />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/MembershipFormforIndividualAnnual" element={<MembershipFormforIndividualAnnual />} />
+        <Route path="/gallery" element={<GalleryPage />} />
       </Routes>
       <Footer />
     </>
